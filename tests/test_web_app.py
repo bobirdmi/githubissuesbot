ace924c9175ce77509a934e787a56b5c51734935
Dmitriy Bobir <astercassio@gmail.com>
2016-10-28 19:17:43 +0200

    Added web app test of main page

---
+++ tests/test_web_app.py
@@ -0,0 +1,15 @@
+import pytest
+import markdown
+from githubissuesbot.web_app import app
+
+
+@pytest.fixture
+def testapp():
+    app.config['TESTING'] = True
+    return app.test_client()
+
+
+def test_main_page(testapp):
+    with open('README.md', 'r') as f:
+        assert markdown.markdown(f.read()) in testapp.get('/').data.decode('utf-8')
+
